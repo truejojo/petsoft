9469684bdde94e30d643da0c9ee43af633c4eaa4
truejojo <johannesveh@yahoo.de>
2025-07-18 14:00:28 +0200

    Styled Pet-List

--- src/components/petList.tsx
+++ src/components/petList.tsx
@@ -1,11 +1,23 @@
+import Image from 'next/image';
+
 const PetList = () => {
   return (
     <ul className='border-b border-light bg-white'>
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-      <li>Item 4</li>
-      <li>Item 5</li>
+      <li>
+        <button
+          type='button'
+          className='flex items-center w-full cursor-pointer px-5 min-h-[70px] py-1 text-base gap-3 hover:bg-[$EFF1F2] focus:bg-[$EFF1F2] text-wrap'
+        >
+          <Image
+            src='https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png'
+            alt='pet'
+            width={45}
+            height={45}
+            className='rounded-full object-cover'
+          />
+          <p className='font-semibold'>Benjamin</p>
+        </button>
+      </li>
     </ul>
   );
 };
