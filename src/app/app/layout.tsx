e96840f26298e5a011281532dc0a997bad4cf82a
truejojo <johannesveh@yahoo.de>
2025-07-24 10:07:24 +0200

    Get Data from DB-Prisma Client

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -3,15 +3,19 @@ import AppFooter from '@/components/app/footer';
 import AppHeader from '@/components/app/header';
 import BackgroundImage from '@/components/backgroundImage';
 import SearchPetContextProvider from './context/searchPetContextProvider';
+import { prisma } from '@/lib/db';
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  const response = await fetch(
-    'https://bytegrad.com/course-assets/projects/petsoft/api/pets',
-  );
-  if (!response.ok) {
-    throw new Error('Failed to fetch pets');
-  }
-  const data = await response.json();
+  // const response = await fetch(
+  //   'https://bytegrad.com/course-assets/projects/petsoft/api/pets',
+  // );
+  // if (!response.ok) {
+  //   throw new Error('Failed to fetch pets');
+  // }
+  // const data = await response.json();
+
+  // pet by prisma.pet is the name of the model in lowercase from schema.prisma
+  const data = await prisma.pet.findMany();
 
   return (
     <>
