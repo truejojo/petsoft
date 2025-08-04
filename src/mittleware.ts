185219a4b3d8fcf8668aa9014684110bf6997db0
truejojo <johannesveh@yahoo.de>
2025-08-04 14:09:47 +0200

    add middleware.ts

---
+++ src/mittleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+
+export const middleware = (request: Request) => {
+  console.log(request.url);
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+};
